432ca113f2c3b91a1389f831bdab2eeecc3fd8e1
Arthur <68960780+4rthurAP@users.noreply.github.com>
2022-04-01 21:26:05 -0300

    commit classes

---
+++ BackEnd/BackEnd.Cervejaria/SofwareContext/Cervejaria.Domain/Common/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using Cervejaria.Domain.Enuns;
+
+namespace Cervejaria.Domain.Common
+{
+    public record Usuario
+    {
+        public Usuario()
+        {
+
+        }
+        public int Id { get; init; }
+        public string Nome { get; init; }
+        public string CNPJ { get; init; }
+        public string IE { get; init; }
+        public TipoUsuario? Tipo { get; init; }
+        public int? EnderecoId { get; init; }
+    }
+}
