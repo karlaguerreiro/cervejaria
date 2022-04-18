51cd7b3ae1c82a7f5c00d2489071d40d4d041dca
Karla Guerreiro <59694318+karlaguerreiro@users.noreply.github.com>
2022-04-18 20:19:14 -0300

    Ajustes de CSS por conta da instalação do bootstrap 5
    Ajustes de CSS por conta da instalação do bootstrap 5

--- FrontEnd/Cervejaria.FrontEnd/ClientApp/src/app/app.module.ts
+++ FrontEnd/Cervejaria.FrontEnd/ClientApp/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './views/home/home.component';
 import { MenuComponent } from './component/menu/menu.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ListItemComponent } from './component/list-item/list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
