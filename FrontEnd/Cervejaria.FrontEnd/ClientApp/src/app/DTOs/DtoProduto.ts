ed883a758c3b957873f7934168560285984d25e7
Douglas <dougl4s.sales@gmail.com>
2022-05-22 21:09:07 -0300

    Fix add_produto

---
+++ FrontEnd/Cervejaria.FrontEnd/ClientApp/src/app/DTOs/DtoProduto.ts
@@ -0,0 +1,9 @@
+export interface DtoProduto {
+    id?: number;
+    nome: string;
+    descricao: string;
+    fabricacao: number;
+    validade: number;
+    quantidade: number;
+    unidadeMedida: string;
+}
