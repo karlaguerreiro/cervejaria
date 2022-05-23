eba58b066509e5b8631be00ab0dc36a7843e9653
Karla Guerreiro <59694318+karlaguerreiro@users.noreply.github.com>
2022-05-23 19:18:27 -0300

    Arrumando os erros do meu repositorio

--- FrontEnd/Cervejaria.FrontEnd/ClientApp/src/app/Service/receita.service.ts
+++ FrontEnd/Cervejaria.FrontEnd/ClientApp/src/app/Service/receita.service.ts
@@ -1,31 +1,9 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from 'rxjs';
-import { BaseResponse } from '../Models/BaseResponse';
-import { Receita } from '../Models/Receita';
-
 @Injectable({
   providedIn: 'root'
 })
 export class ReceitaService {
-  constructor(private http: HttpClient) { }
-  protected UrlServiceV1: string = "https://cervejaria-api.herokuapp.com/";
 
-  headerOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-  }
-  public obterReceitas() : Observable< BaseResponse<Receita[]>> {
-    return this.http.get<BaseResponse<Receita[]>>(this.UrlServiceV1 + "api/V1/Receita");
-  }
-
-  public inserirReceitas(args: any) : Observable<any> {
-    return this.http.post<any>(this.UrlServiceV1 + "api/V1/Receita", args, this.headerOptions);
-  }
-
-
+  constructor() { }
 }
-
-
